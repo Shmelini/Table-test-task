808caff1528f3f640b2900feca2ee9e7cde34ee0
Artem Bondarchuk <arhipilagys@gmail.com>
2023-11-25 19:10:46 +1100

    very last changes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from "vue";
 import App from './App'
-// import components from ''
+
 
 
 createApp(App).mount('#app')
 
-// components.forEach(component => {
-//     app.component(component.name, component)
-// })
